b78bb3ba8ab8ed03fade13b0ed334c5162ffa6ce
pimkww <pimkww@gmail.com>
2023-06-13 21:32:37 +0300

    removed extra files

--- tesla-clone/src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import carReduser from "../features/car/carSlice"
-export const store = configureStore({
-  reducer: {
-    car: carReduser,
-  },
-});
